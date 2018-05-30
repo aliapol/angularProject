1cc98fd1ceada6176cfb0d4b01beed9e772bd5b0
aliapol <polsgro2@gmail.com>
2018-05-30 16:53:57 -0400

    added icons to movielist.js template

--- movieList.js
+++ movieList.js
@@ -8,10 +8,13 @@ const movieList = {
     <section id="moviebox" ng-repeat="movie in $ctrl.movies.$$state.value.data.results">
     <img class="poster" ng-src=" https://image.tmdb.org/t/p/w185/{{movie.poster_path}}"> 
     <div class="movieinfo">
-      <h1> {{movie.title}} </h1>
-      <p> {{movie.overview}} </p>
-    </div>
-     
+    <h1> {{movie.title}} </h1>
+    <p> {{movie.overview}} </p>
+    <i class="material-icons info">info</i>
+    <i class="material-icons off">visibility_off</i>
+    <i class="material-icons on">visibility</i>
+   
+  </div>
 
     </section>
     </section>
