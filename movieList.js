0e929762dc13e486de08271856587a8d49a8c285
aliapol <polsgro2@gmail.com>
2018-05-30 15:12:57 -0400

    added more css and a new section to Movielist.js

--- movieList.js
+++ movieList.js
@@ -4,14 +4,16 @@ const movieList = {
   template: `
   <h1>Name</h1>
     <search-criteria></search-criteria>
+    <section id="movielist">
     <section id="moviebox" ng-repeat="movie in $ctrl.movies.$$state.value.data.results">
-    <img ng-src=" https://image.tmdb.org/t/p/w185/{{movie.poster_path}}"> 
+    <img class="poster" ng-src=" https://image.tmdb.org/t/p/w185/{{movie.poster_path}}"> 
     <div class="movieinfo">
-    <h1> {{movie.title}} </h1>
-     <p> {{movie.overview}} </p>
-     </div>
+      <h1> {{movie.title}} </h1>
+      <p> {{movie.overview}} </p>
+    </div>
      
 
+    </section>
     </section>
 
 
