2c16eaca7848521f966e393822e8d89a8c724ae6
John <ristaujo@gmail.com>
2018-05-31 16:37:26 -0400

    Added info popup

--- movieList.js
+++ movieList.js
@@ -2,19 +2,31 @@
 
 const movieList = {
   template: `
-    <h1>Name</h1>
-    <a href="#!/search">Search</a>
     <section id="movielist">
       <section id="moviebox" ng-repeat="movie in $ctrl.movies.$$state.value.data.results">
         <img class="poster" ng-src=" https://image.tmdb.org/t/p/w185/{{movie.poster_path}}"> 
         <div class="movieinfo">
           <h1> {{movie.title}} </h1>
           <p> {{movie.overview}} </p>
-          <i class="material-icons info">info</i>
+          <i class="material-icons info" ng-click="$ctrl.showPopup(movie);">info</i>
           <i class="material-icons off" ng-click="$ctrl.watchListArray(movie);">visibility_off</i>
           
         </div>
       </section>
+
+      <section id="popup" ng-show="$ctrl.show" >
+        <p ng-click="$ctrl.closePopup();">X</p>
+        <div class="movieinfo">
+          <h3> {{$ctrl.title}}</h3>
+          <h3> {{$ctrl.overview}} </h3>
+          <h3> Rating: {{$ctrl.vote_average}}</h3>
+          <h3> Number of votes: {{$ctrl.vote_count}}</h3>
+          <h3> Release date: {{$ctrl.release_date}}</h3>
+
+
+        </div>
+      </section>
+    
     </section>
   `,
 
@@ -33,9 +45,22 @@ const movieList = {
         console.log(movie);
       };
 
-    
-        
-    
+    vm.showPopup = (movie) => {
+        vm.show = true;
+        vm.title = movie.title;
+        vm.overview = movie.overview;
+        vm.vote_average = movie.vote_average;
+        vm.vote_count = movie.vote_count;
+        vm.release_date = movie.release_date;
+
+
+        console.log(movie.title);
+    };
+    vm.closePopup = () => {
+        vm.show = false;
+    };
+      
+      
 
   }]
 
